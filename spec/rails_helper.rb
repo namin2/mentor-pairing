9eac210447de947f47ec94856ffaa8ff3905e363
DJ DeWitt & Nirag Amin <pair+djdewitt.namin2@devbootcamp.com>
2015-01-20 17:00:31 -0600

    requiring time helper for spec

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -3,7 +3,7 @@ ENV["RAILS_ENV"] ||= 'test'
 require 'spec_helper'
 require File.expand_path("../../config/environment", __FILE__)
 require 'rspec/rails'
-
+require 'support/fill_in_date_helper.rb'
 require 'coveralls'
 Coveralls.wear!('rails')
 # Add additional requires below this line. Rails is not loaded until this point!
